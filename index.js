8b7c2d17335fab992ceb0d2eaa3a52152fbfe04a
Jack Hawkins <jhawkins@redhat.com>
2018-10-10 16:51:47 +0100

    JSON reading working

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const TeleBot = require('telebot');
 const TOKEN = process.env.TOKEN;
 const bot = new TeleBot(TOKEN);
 
-var acros = JSON.parse(fs.readFileSync('acronyms.json', 'utf8'));
+acroDoc = JSON.parse(fs.readFileSync('acronyms.json', 'utf8'));
+acros = acroDoc.acronyms;
 
 bot.on('/help', (msg) => {
     console.log('/help triggered')
@@ -13,7 +14,8 @@ bot.on('/help', (msg) => {
 =======
 bot.on('/test', (msg) => {
     console.log('/test triggered')
-    var test = acros.ABC;
+    console.log(acros.ABC)
+    test = String(acros.ABC);
     return bot.sendMessage(msg.chat.id,test)
 });
 

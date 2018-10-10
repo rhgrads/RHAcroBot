8b769941380a29a3cb05fd89ad1f70758225987b
Iwan Francis <ifrancis@ifrancis.remote.csb>
2018-10-10 16:17:21 +0100

    Basic implementation of bot

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const TeleBot = require('telebot');
-const Promise = require('bluebird');
 const TOKEN = process.env.TOKEN;
 const bot = new TeleBot(TOKEN);
-const fs = require('fs');
 
 bot.on('/help', (msg) => {
     console.log('/help triggered')
-    return bot.sendMessage(
-        msg.chat.id(' I am the Red Hat Acrobot, I help decypher acronyms used within the company.'
-        )
-    );
+    return bot.sendMessage(msg.chat.id,' I am the Red Hat Acrobot, I help decypher acronyms used within the company.')
 });
+
+bot.on('/ac', (msg) => {
+    console.log('/ac triggered')
+    return bot.sendMessage(msg.chat.id, 'Yo')
+});
+
+bot.start()

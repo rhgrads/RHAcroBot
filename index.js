80bf9a690e372532e3a49d0af4c1bed9889cba5d
Iwan Francis <iwan@redhat.com>
2018-10-19 13:22:39 +0100

    Made help text and responses more user friendly * Shifted acroLookup function up above command definitions. * Adjusted /help text. * Made failure message more polite. * Removed /test command.

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ const bot = new TeleBot(TOKEN);
 const fs = require('fs');
 acroDoc = JSON.parse(fs.readFileSync('acronyms.json', 'utf8'));
 
-bot.on('/help', (msg) => {
-    console.log('/help triggered')
-    return bot.sendMessage(msg.chat.id,' I am the Red Hat Acrobot, I help decypher acronyms used within the company.')
-});
-
 function acroLookup(input){
     output = null;
     cats = ["acronyms", "redhat", "cryptography", "virt", "networking", "wifi", "libreoffice", "selinux", "kernal", "cloud", "documentation", "standards", "openshift", "gss", "hss", "pciutils", "java", "do_not_use", "memory_mgmt", "salesforce", "tags","eggs", "consulting", "new"];
@@ -22,6 +17,12 @@ function acroLookup(input){
       return output;
 }
 
+bot.on('/help', (msg) => {
+    console.log('/help triggered')
+    return bot.sendMessage(msg.chat.id,
+        "I am the Red Hat Acrobot, I help decypher acronyms used within the company! \n\nTo search for an acronym, type /ac followed by the acronym.")
+});
+
 bot.on(/^\/ac (.+)$/, (msg, props) => {
     console.log('/ac triggered')
     console.log(props.match[1])
@@ -33,15 +34,8 @@ bot.on(/^\/ac (.+)$/, (msg, props) => {
         return bot.sendMessage(msg.chat.id,test)
     }
     else{
-        return bot.sendMessage(msg.chat.id, 'Acronym not found.')
+        return bot.sendMessage(msg.chat.id, "I'm sorry, I couldn't find that acronym.")
     }
-});
-
-
-bot.on('/test', (msg) => {
-
-    console.log('/test triggered')
-    return bot.sendMessage(msg.chat.id, 'Yo')
 });
 
 bot.start()
